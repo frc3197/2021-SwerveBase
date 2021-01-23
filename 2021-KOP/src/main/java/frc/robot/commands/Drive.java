fc123e4770a68da03b8029884b4c93860c34b4a4
YuriKleyman <kleymanyy@gmail.com>
2021-01-22 20:48:15 -0600

    ok dis hayden but it kinda work ok byeee
    13% of populatiobn

---
+++ 2021-KOP/src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveTrain;
+
+public class Drive extends CommandBase {
+  DriveTrain drivetrain;
+  /** Creates a new Drive. */
+  public Drive(DriveTrain drivetrain) 
+  {
+    this.drivetrain = drivetrain;
+    addRequirements(drivetrain);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double l = RobotContainer.getYLeftStick();
+    double r = RobotContainer.getYRightStick();
+    drivetrain.tankDrive(l, r);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
